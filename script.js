9d03354c621483f7442b9dd04c6ceb5c743bfd82
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2025-08-09 08:14:07 +0000

    2tabs

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ const retetar = document.createElement("div");
 retetar.id = "retetar";
 retetar.innerHTML = `Hello retetar`;
 app.appendChild(retetar);
+
+const regimuri = document.createElement("div");
+regimuri.id = "regimuri";
+regimuri.innerHTML = `Hello regimuri`;
+app.appendChild(regimuri);
