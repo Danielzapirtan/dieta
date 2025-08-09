9de05f686bbe40033c6a4b3e45eeeca62ebdbfb7
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2025-08-09 02:32:53 +0000

    m

---
+++ script.js
@@ -0,0 +1,9 @@
+retetar = {
+	[], // mancaruri
+	[] // retete
+};
+
+const retetar = document.createElement('div');
+retetar.id = "retetar";
+retetar.innerHTML = `Acesta este un exemplu de retetar`;
+
